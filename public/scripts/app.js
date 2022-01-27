fc40e2f1d1a1588b910f491b7fb192b8d31a81ff
Joey Steigelman <joey.steigelman@gmail.com>
2022-01-27 12:17:16 -0500

    installed react and react-dom

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,11 @@
 "use strict";
 
-var _validator = _interopRequireDefault(require("validator"));
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-console.log(_validator["default"].isEmail('test'));
+var template = /*#__PURE__*/_react["default"].createElement('p', {}, 'testing testing');
+
+_reactDom["default"].render(template, document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
-import validator from 'validator'
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-console.log(validator.isEmail('test'))
+const template= React.createElement('p', {}, 'testing testing')
+ReactDOM.render(template, document.getElementById('app'))
